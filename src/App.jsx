dc5d886f1494812d1256902d619e904967e25d07
Juan Etcheverry <125306131+JuanEtcheverry2002@users.noreply.github.com>
2023-05-10 19:38:54 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             <ItemDetailContainer />
           </Route>
           <Route path="/cart">
-            <Cart />
+            
           </Route>
      
       </div>
