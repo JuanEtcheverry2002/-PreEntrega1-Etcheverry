5f157668a6b19ff5e27ebc37fe44548a1bae2e4f
Juan Etcheverry <125306131+JuanEtcheverry2002@users.noreply.github.com>
2023-05-15 18:39:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import ItemListContainer from './Components/ItemListContainer';
 import { BrowserRouter,Routes,Route,Router } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import ItemDetailContainer from "./Components/ItemDetailContainer";
+import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
   return (
